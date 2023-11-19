5f8d28adec8f78f10848dcc988818f45ce179815
Oscar Perona <144930013+peronaOscar@users.noreply.github.com>
2023-11-19 16:42:50 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("E F B D G C B A ", 422), music.PlaybackMode.UntilDone)
+})
